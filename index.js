94aa7597b08abf3d39962896d018640067c2ed2e
JarikiSungnyung <hyuk0496@gmail.com>
2023-02-23 00:22:04 +0900

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: "https://web-kktudic-frontend-r8xoo2mlefs9qly.sel3.cloudtype.app/",
+    origin: "https://web-kktudic-frontend-r8xoo2mlefs9qly.sel3.cloudtype.app",
   })
 );
 
