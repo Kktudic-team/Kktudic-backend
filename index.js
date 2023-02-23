62fadf7f6cc595ea57ff9c8796394e035a1e878c
JarikiSungnyung <hyuk0496@gmail.com>
2023-02-24 02:01:55 +0900

    update

--- index.js
+++ index.js
@@ -1,69 +1,71 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const port = 3000;
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
 
-app.use(
-  cors({
-    origin: "http://localhost:8080",
-  })
-);
+app.use(cors({
+  origin: 'http://localhost:8080'
+}))
 
 app.listen(port, () => {
-  console.log(`Running on http://localhost:${port}`);
-});
+  console.log(`Running on http://localhost:${port}`)
+})
 
-const fs = require("fs");
-let words = fs.readFileSync("words.txt", "utf-8").split("\n");
-
-const sortWordsByLength = (words) => {
-  return words.sort((a, b) => b.length - a.length);
+const fs = require('fs');
+// 중복 단어를 제거하는 함수
+const removeDuplicates = arr => {
+  return [...new Set(arr.map(word => word.trim()))];
 };
 
-words = sortWordsByLength(words);
+function sortword() {
+  let words = fs.readFileSync('words.txt', 'utf-8').split('\n');
 
-fs.writeFileSync("words.txt", words.join("\n"));
+  // 중복 단어 제거
+  words = removeDuplicates(words);
+  const sortWordsByLength = words => {
+    return words.sort((a, b) => {
+      if (a.length === b.length) {
+        return a.localeCompare(b);
+      } else {
+        return b.length - a.length;
+      }
+    });
+  };
 
-const word = [];
+  words = sortWordsByLength(words);
+
+  fs.writeFileSync('words.txt', words.join('\n'));
+}
+sortword();
 
 const getWords = (firstLetter, secondLetter) => {
-  let time = 0;
+  let time = 0
   const startTime = Date.now();
-  let filteredWords = fs.readFileSync("words.txt", "utf-8").split("\n");
-  filteredWords = filteredWords.filter((word) =>
-    word.startsWith(firstLetter.toString().trim())
-  );
+  let filteredWords = fs.readFileSync('words.txt', 'utf-8').split('\n');
+  filteredWords = filteredWords.filter(word => word.startsWith(firstLetter.toString().trim()));
   let sortedWords = [];
   let words = [];
   let count = [];
   if (secondLetter && secondLetter.trim()) {
-    sortedWords = filteredWords.sort(
-      (a, b) =>
-        b.split(secondLetter.toString().trim()).length -
-        a.split(secondLetter.toString().trim()).length
-    );
+    sortedWords = filteredWords.sort((a, b) => b.split(secondLetter.toString().trim()).length - a.split(secondLetter.toString().trim()).length);
   } else {
     sortedWords = filteredWords;
   }
-  sortedWords.forEach((word) => {
+  sortedWords.forEach(word => {
     let c = 0;
-    if (
-      secondLetter &&
-      secondLetter.trim() &&
-      word.includes(secondLetter.toString().trim())
-    ) {
+    if (secondLetter && secondLetter.trim() && word.includes(secondLetter.toString().trim())) {
       c = word.split(secondLetter.toString().trim()).length - 1;
     }
     words.push(word);
     count.push(c);
   });
-  time = (Date.now() - startTime) / 1000;
+  time = (Date.now() - startTime) / 1000
   return [[words, count], time];
 };
 
 app.use(express.json());
 
-app.post("/api/submit", (req, res) => {
+app.post('/api/submit', (req, res) => {
   try {
     const [result, time] = getWords(req.body.startWord, req.body.missionWord);
     res.send({ word: result[0], count: result[1], time: time });
@@ -73,20 +75,20 @@ app.post("/api/submit", (req, res) => {
   }
 });
 
-app.get("/api/notices", (req, res) => {
-  fs.readFile("notice.txt", "utf8", (err, data) => {
+app.get('/api/notices', (req, res) => {
+  fs.readFile('notice.txt', 'utf8', (err, data) => {
     if (err) {
       res.status(500).send(err);
       return;
     }
 
-    const notices = data.split("\r\n\r\n");
+    const notices = data.split('\r\n\r\n');
     const titles = [];
     const details = [];
     const dates = [];
 
     notices.forEach((notice) => {
-      const [title, ...contentDate] = notice.split("\n");
+      const [title, ...contentDate] = notice.split('\n');
       const [content, date] = contentDate;
       titles.push(title);
       details.push(content);
@@ -103,15 +105,15 @@ app.get("/api/notices", (req, res) => {
   });
 });
 
-app.get("/api/notices/:id", (req, res) => {
+app.get('/api/notices/:id', (req, res) => {
   const id = Number(req.params.id);
-  fs.readFile("notice.txt", "utf8", (err, data) => {
+  fs.readFile('notice.txt', 'utf8', (err, data) => {
     if (err) {
       res.status(500).send(err);
       return;
     }
 
-    const notices = data.split("\r\n\r\n");
+    const notices = data.split('\r\n\r\n');
     if (id < 1 || id > notices.length) {
       res.status(404).send(`Notice with ID ${id} not found`);
       return;
@@ -122,23 +124,23 @@ app.get("/api/notices/:id", (req, res) => {
     const dates = [];
 
     notices.forEach((notice) => {
-      const [title, ...contentDate] = notice.split("\n");
+      const [title, ...contentDate] = notice.split('\n');
       let [content, date] = contentDate;
       if (!date) {
-        date = "";
+        date = '';
       }
       titles.push(title);
-      details.push(content);
+      details.push(content.replace(/\n/g, '<br>')); // 개행 문자를 <br> 태그로 대체
       dates.push(date.trim());
     });
 
     const notice = notices[id - 1];
-    const [title, content, createdAt] = notice.split("\n");
+    const [title, content, createdAt] = notice.split('\n');
     const result = {
       id,
       title,
       content,
-      createdAt: createdAt.trim(),
+      createdAt: createdAt.trim()
     };
     res.json(result);
   });
